84b5cae7ea22b9a0ae350708602689289518babc
MehmetKagan <mehmetkagankokoglu@gmail.com>
2023-03-07 00:31:55 +0300

    fix: Fix ReferenceError and Add all posibilities

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ let computerChoice = getComputerChoice();
 
 let playerSelection = prompt("Rock, Paper or Scissors?");
 
-if (playerSelection.toLowerCase = "rock") {
-    if (computerChoice = "Paper") { alert("You Lose! Paper beats Rock") }
-    else if (computerChoice = "Scissors") { alert("You Win! Rock beats Scissors") }
-    else { alert ("It's  a tie!")}
+if (playerSelection.toLowerCase() === "rock") {
+    if (computerChoice === "Paper") { alert("You Lose! Paper beats Rock") }
+    else if (computerChoice === "Scissors") { alert("You Win! Rock beats Scissors") }
+    else { alert("It's  a tie!") }
 }
+else if (playerSelection.toLowerCase() === "paper") {
+    if (computerChoice === "Scissors") { alert("You Lose! Scissors beats paper!") }
+    else if (computerChoice === "Rock") { alert("You Win! Paper beats Rock") }
+    else { alert("It's  a tie!") }
+}
+    
+else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerChoice === "Rock") { alert("You Lose! Rock beats scissors!") }
+        else if (computerChoice === "Paper") { alert("You Win! Scissors beats Rock") }
+        else { alert("It's  a tie!") }
+}
+    else {alert ("Please enter a valid choice.")}
